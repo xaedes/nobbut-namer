7c1efc2ae437ec62128fa07aa6379fb8e6d6c80e
xaedes <xaedes@gmail.com>
2022-10-06 05:17:03 +0200

    improve quality and speed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nobbut_namer
+---
+Generate names from keywords.
+This project generated its own name.
+Code of nobbut_namer was made with copilot.
--- nobbut_namer/main.py
+++ nobbut_namer/main.py
@@ -4,8 +4,9 @@ import os
 import requests
 import string
 import itertools
-import pickle
+import math
 import random
+import numpy as np
 from progress.bar import Bar
 
 
@@ -26,7 +27,7 @@ def main():
     parser.add_argument("-v", "--vocabulary", help="vocabulary file", type=str, default="vocabulary.txt")
     parser.add_argument("-t", "--top", help="top n words to use", type=int, default=1000)
     parser.add_argument("--sep", help="separator between words in name", type=str, default="-")
-    parser.add_argument("--cache", help="cache file", type=str, default="embeddings_cache.pkl")
+    parser.add_argument("--cache", help="cache file", type=str, default="embeddings_cache.npz")
     args = parser.parse_args()
 
     if len(args.keywords) == 0:
@@ -46,41 +47,34 @@ def main():
     print("sep".ljust(15), args.sep)
     print("cache".ljust(15), args.cache)
 
+    print("Loading cache...")
+    #cache = Cache(args.cache)
 
-    cache = Cache(args.cache)
+    print("Loading model...")
+    text_model = TextEmbeddingModel()
+    get_text_embedding = text_model.get_text_embedding
 
+    # the vocabulary is a list of words that are used to generate names.
+    # if it does not exist, a list of words suitable for name generation is downloaded and used to generate the vocabulary.
+    # if the vocabulary exists, it is loaded and used to generate names.
+
+    print("Loading vocabulary...")
+    vocabulary = get_vocabulary(args.vocabulary)
+    
+    print("Embedding vocabulary...")
+    voc_embeddings = get_vocabulary_embeddings(get_text_embedding, vocabulary, args.cache, text_model.device)
+    
     # generate names and print them.
+    names = generate_names(
+        get_text_embedding,
+        vocabulary, voc_embeddings,
+        args.keywords, 
+        args.count, args.size, args.top, args.sep,
+    )
+
     print("Generated names:")
-    for name in generate_names(cache, args.keywords, args.count, args.size, args.vocabulary, args.top, args.sep):
+    for name in names:
         print(name)
-
-
-class Cache:
-    def __init__(self, filename):
-        self.filename = filename
-        self.cache = {}
-        
-        # load cached embeddings from file if they exist.
-        # use pickle
-        if os.path.exists(self.filename):
-            with open(self.filename, "rb") as f:
-                self.cache = pickle.load(f)
-
-    def save(self):
-        with open(self.filename, "wb") as f:
-            pickle.dump(self.cache, f)
-
-    def cached(self, fn):
-        # cache the result of a function call.
-        # the function call is cached using the function parameters as key.
-        # the function call is only executed if the result is not cached.
-        # the result of the function call is returned.
-        cache = self.cache
-        def wrapper(*args):
-            if args not in cache:
-                cache[args] = fn(*args)
-            return cache[args]
-        return wrapper
 
 def get_vocabulary(filename):
     # get the vocabulary from the file if it exists.
@@ -154,12 +148,36 @@ def get_vocabulary(filename):
         vocabulary = words
     
     return vocabulary
+    
+def get_vocabulary_embeddings(get_text_embedding, vocabulary, cache_filename, device, batch_size=128):
+    try:
+        # try to load vocabulary embeddings from cache.
+        res = np.load(cache_filename)
+        voc_embeddings = res["voc_embeddings"]
+        if voc_embeddings.shape[0] != len(vocabulary):
+            raise ValueError("vocabulary size mismatch")
+        # convert to torch tensor.
+        voc_embeddings = torch.from_numpy(voc_embeddings).to(device)
+        return voc_embeddings
+    except:
+        # split vocabulary into batches
+        voc_batches = [vocabulary[i:min(i+batch_size,len(vocabulary))] for i in range(0, len(vocabulary), batch_size)]
+        if len(voc_batches) > 0 and len(voc_batches[-1]) < batch_size:
+            voc_batches[-1] += [""] * (batch_size - len(voc_batches[-1]))
+        # use batches to generate embeddings.
+        voc_embeddings = list(Bar(max=len(voc_batches)).iter(map(get_text_embedding, voc_batches)))
+        voc_embeddings = torch.stack(voc_embeddings)
+        voc_embeddings = voc_embeddings.reshape(-1,voc_embeddings.shape[-1])
+        voc_embeddings = voc_embeddings[:len(vocabulary)]
+        np.savez(cache_filename, voc_embeddings=voc_embeddings.cpu().numpy())
+        return voc_embeddings
+
 
 # use a pretrained pytorch model from huggingface to generate text embeddings.
 # the model is initialized when the class TextEmbeddingModel is instantiated.
 # the pretrained model is stored in a cache folder and automatically downloaded 
 # if it does not exist.
-# we use huggingface for the pretrained model which required no authentication token.
+# we use huggingface for the pretrained model which requires no authentication token.
 # using the function TextEmbeddingModel.get_text_embedding(self, text) the embedding 
 # of a text can be generated.
 
@@ -181,79 +199,91 @@ class TextEmbeddingModel:
         # the tokens are converted to the model's input format.
         # the tokens are passed to the model to generate the text embedding.
         # the text embedding is returned.
-        tokenized_text = self.tokenizer(text, return_tensors="pt")
+        if type(text) == list:
+            tokenized_text = self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+        else:
+            tokenized_text = self.tokenizer(text, return_tensors="pt")
         tokenized_text = {key: tokenized_text[key].to(self.device) for key in tokenized_text}
         with torch.no_grad():
             text_embedding = self.model(**tokenized_text).pooler_output
         return text_embedding
 
-def generate_names(cache, keywords, count, size, vocabulary_filename, top, sep):
-    # generate names using the keyword as seed.
-    # the names are generated using the text embedding of the keyword.
-    # the text embedding is used to find the nearest words in the vocabulary.
-    # the vocabulary is a list of words that are used to generate names.
-    # if it does not exist, a list of words suitable for name generation is downloaded and used to generate the vocabulary.
-    # if the vocabulary exists, it is loaded and used to generate names.
-    print("loading model...")
-    text_model = TextEmbeddingModel()
-    get_text_embedding = cache.cached(text_model.get_text_embedding)
+def generate_names(get_text_embedding, vocabulary, voc_embeddings, keywords, count, size, top, sep):
+    with torch.no_grad():
+        # generate names using the keyword as seed.
+        # the names are generated using the text embedding of the keyword.
+        # the text embedding is used to find the nearest words in the vocabulary.
 
-    print("loading vocabulary...")
-    vocabulary = get_vocabulary(vocabulary_filename)
-    
-    print("embedding vocabulary...")
-    voc_embeddings = list(Bar(max=len(vocabulary)).iter(map(get_text_embedding, vocabulary)))
-    
-    print("embedding keywords...")
-    text_embeddings = list(Bar(max=len(keywords)).iter(map(get_text_embedding, keywords)))
-    text_embeddings = torch.stack(text_embeddings)
-    cache.save()
-    names = []
-    
-    # generate word tuples from the vocabulary by picking two words from the vocabulary.
-    # sort vocabulary by minimum similarity to on of the keywords and randomly pick from the top 100.
-    # use compare_embeddings() to compare the similarity of text embeddings.
-    def compare_embeddings(a, b):
-        # compare by similarity of two text embeddings.
-        # the embeddings are compared using cosine similarity.
-        # the similarity is returned.
-        return 1-torch.nn.functional.cosine_similarity(a, b).item()
+        print("embedding keywords...")
+        text_embeddings = list(Bar(max=len(keywords)).iter(map(get_text_embedding, keywords)))
+        #text_embeddings = torch.stack(text_embeddings)
+        #print("text_embeddings.shape:", text_embeddings.shape)
 
-    def compare_embeddings_multi(a, b_multi):
-        # compare by similarity between a text embedding and multiple text embeddings.
-        # the embeddings are compared using cosine similarity.
-        # the combined similarity is returned.
-        return torch.nn.functional.cosine_similarity(a, b_multi).mean().item()
-    
-    print("generating names...")
-    # find top words in vocabulary that are most similar to on of the keywords.
-    similarities = [
-        compare_embeddings_multi(voc_embedding, text_embeddings)
-        for voc_embedding in voc_embeddings
-    ]
-    top_words = sorted(
-        zip(vocabulary,similarities), 
-        key=lambda word_sim: word_sim[1]
-    )[::-1][:top]
-    top_word_similarities = [word_sim[1] for word_sim in top_words]
-    top_words = [word_sim[0] for word_sim in top_words]
-    # generate names by randomly picking two words from the top 100 words.
-    for words in random_combinations(top_words, top_word_similarities, size, count):
-        names.append(sep.join(words))
-    
-    # return names.
-    return names
+        
+        # generate word tuples from the vocabulary by picking two words from the vocabulary.
+        # sort vocabulary by minimum similarity to on of the keywords and randomly pick from the top 100.
+        # use compare_embeddings() to compare the similarity of text embeddings.
+        def compare_embeddings(a, b):
+            # compare by similarity of two text embeddings.
+            # the embeddings are compared using cosine similarity.
+            # the similarity is returned.
+            return 1-torch.nn.functional.cosine_similarity(a, b).item()
 
-def random_combinations(values, weights, r, count):
+        def compare_embeddings_multi(a, b_multi):
+            # compare by similarity between a text embedding and multiple text embeddings.
+            # the embeddings are compared using cosine similarity.
+            # the combined similarity is returned.
+            return torch.nn.functional.cosine_similarity(a, b_multi)
+        
+        
+        # find top words in vocabulary that are most similar to on of the keywords.
+        print("score similarities of vocabulary to keywords...")
+        similarities = list(Bar(max=len(text_embeddings)).iter((
+            compare_embeddings_multi(
+                text_embedding.reshape(text_embedding.shape[1]), 
+                voc_embeddings
+            )
+            for text_embedding in text_embeddings
+        )))
+        
+        # convert list of tensors to tensor
+        similarities = torch.stack(similarities)
+        similarities = similarities.mean(dim=0)
+
+        # convert tensor to numpy array
+        similarities = similarities.cpu().numpy()
+
+        top_words = sorted(
+            zip(vocabulary,similarities), 
+            key=lambda word_sim: word_sim[1]
+        )[::-1][:top]
+        top_word_similarities = [word_sim[1] for word_sim in top_words]
+        top_words = [word_sim[0] for word_sim in top_words]
+        print("generating names...")
+        # generate names by randomly picking two words from the top 100 words.
+        combinations_limit = top*2
+        names = []
+        for words in random_combinations(top_words, top_word_similarities, size, count, combinations_limit):
+            names.append(sep.join(words))
+        
+        # return names.
+        return names
+
+def random_combinations(values, weights, r, count, combinations_limit):
     # generate a list of random combinations of length r from the iterable.
     # the list contains count combinations.
     # the combinations are generated using the python package "itertools".
     # the combinations are returned.
-    combinations = list(itertools.combinations(range(len(values)), r))
+    
+    # compute the number of combinations.
+    num_combinations = int(math.factorial(len(values)) / (math.factorial(r) * math.factorial(len(values)-r)))
+    combinations = list(Bar(max=num_combinations).iter(
+        itertools.combinations(range(len(values)), r),
+    ))
     if count < len(combinations):
         # randomly pick combinations from the weighted list of combinations.
         combinations_weights = [
-            max(weights[i] for i in combination)
+            np.product([weights[i] for i in combination])
             for combination in combinations
         ]
         combinations = random.choices(combinations, weights=combinations_weights, k=count)
@@ -263,3 +293,7 @@ def random_combinations(values, weights, r, count):
         for combination in combinations
     ]
 
+def list_atmost(iterable, count):
+    # return a list of at most count elements from the iterable.
+    # the list is returned.
+    return list(itertools.islice(iterable, count))
